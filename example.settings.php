eff98c45d7f078215f90ec994480d46664437d56
ACH1980 <m.hoffmann@kanjias.de>
2020-04-22 13:47:14 +0200

    example file added

--- example.settings.php
+++ example.settings.php
@@ -4,6 +4,7 @@
         static $tokenLength = 255; // set the token length here
         static $authRequired = true; // set to false if you don't require a token for app registration - really not recommended!
         static $auth_tokens = ["Token1", "Token2" /* etc. */]; // list of the tokens that are authenticated to create Bots. Can be any string, but we recommend to use something secure alphanumeric (but not your passwords, because this is in clear text on your server)
-
+        static $db_host = "localhost"; // Host of the database
+        static $db_name = "{Name}"; // Name of the database
     }
 ?>
